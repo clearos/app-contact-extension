d5950f4ec8da1bb6a18cfa7c2ea7f95f3e155107
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:25:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Ny Contact Extension dia nanitatra ny lahatahiry amin&#39;ny mpampiasa vaovao fifandraisana toy ny finday maro, finday, sy ny adiresy.';
+$lang['contact_extension_app_name'] = 'Contact Extension';
+$lang['contact_extension_contact_account_extension'] = 'Contact Account Extension';
+$lang['contact_extension_contact'] = 'Contact';
+$lang['contact_extension_city'] = 'City';
+$lang['contact_extension_country'] = 'Firenena';
+$lang['contact_extension_fax_number'] = 'Fax Number';
+$lang['contact_extension_mobile_number'] = 'Nomerao finday';
+$lang['contact_extension_organization'] = 'fikambanana';
+$lang['contact_extension_postal_code'] = 'Kaody postaly';
+$lang['contact_extension_post_office_box'] = 'Postal Office Box';
+$lang['contact_extension_region'] = 'Region';
+$lang['contact_extension_room_number'] = 'Room Number';
+$lang['contact_extension_street'] = 'Street';
+$lang['contact_extension_telephone_number'] = 'Telephone Number';
+$lang['contact_extension_unit'] = 'Unit';
+$lang['contact_extension_city_is_invalid'] = 'City dia tsy manan-kery.';
+$lang['contact_extension_country_is_invalid'] = 'Firenena dia tsy manan-kery.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Fax isa no tsy manan-kery.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Mobile isa no tsy manan-kery.';
+$lang['contact_extension_organization_is_invalid'] = 'Fikambanana dia tsy manan-kery.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Paositra efajoro dia tsy manan-kery.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Ny haavon&#39;ny dia tsy manan-kery.';
+$lang['contact_extension_region_is_invalid'] = 'Faritra dia tsy manan-kery.';
+$lang['contact_extension_room_number_is_invalid'] = 'Room isa no tsy manan-kery.';
+$lang['contact_extension_street_is_invalid'] = 'Street dia tsy manan-kery.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Telefaonina isa no tsy manan-kery.';
+$lang['contact_extension_unit_is_invalid'] = 'Unit dia tsy manan-kery.';
