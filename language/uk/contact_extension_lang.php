2f4b2f8f3b53b41cbae6007aec06f91ccf9d12a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:56:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Контактна Extension розширює каталог з користувачем контактною інформацією, такий як номер телефону, мобільний, і адреса.';
+$lang['contact_extension_app_name'] = 'Зв&#39;язок з Extension';
+$lang['contact_extension_contact_account_extension'] = 'Зв&#39;язок з Extension Account';
+$lang['contact_extension_contact'] = 'контакт';
+$lang['contact_extension_city'] = 'місто';
+$lang['contact_extension_country'] = 'країна';
+$lang['contact_extension_fax_number'] = 'Номер факсу';
+$lang['contact_extension_mobile_number'] = 'Номер мобільного';
+$lang['contact_extension_organization'] = 'організація';
+$lang['contact_extension_postal_code'] = 'Поштовий індекс';
+$lang['contact_extension_post_office_box'] = 'Поштове відділення Box';
+$lang['contact_extension_region'] = 'область';
+$lang['contact_extension_room_number'] = 'Номер кімнати';
+$lang['contact_extension_street'] = 'вулиця';
+$lang['contact_extension_telephone_number'] = 'Номер телефону';
+$lang['contact_extension_unit'] = 'блок';
+$lang['contact_extension_city_is_invalid'] = 'Місто є недійсним.';
+$lang['contact_extension_country_is_invalid'] = 'Країна є недійсним.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Номер факсу є недійсним.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Мобільний телефон є недійсним.';
+$lang['contact_extension_organization_is_invalid'] = 'Організація є недійсним.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Поштова скринька є недійсним.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Поштовий індекс є недійсним.';
+$lang['contact_extension_region_is_invalid'] = 'Регіон є недійсним.';
+$lang['contact_extension_room_number_is_invalid'] = 'Кількість номерів є недійсним.';
+$lang['contact_extension_street_is_invalid'] = 'Вулиця є недійсним.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Номер телефону є недійсним.';
+$lang['contact_extension_unit_is_invalid'] = 'Одиниця є недійсним.';
