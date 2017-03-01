eba3c6175b9789927625e0f0d232e322703b211c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:09:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'The Kpọtụrụ Extension gbatịrị na ndekọ na onye ọrụ ozi kọntaktị dị ka nọmba ekwentị, mobile, na adreesị.';
+$lang['contact_extension_app_name'] = 'Kpọtụrụ Extension';
+$lang['contact_extension_contact_account_extension'] = 'Kpọtụrụ Account Extension';
+$lang['contact_extension_contact'] = 'Kpọtụrụ';
+$lang['contact_extension_city'] = 'City';
+$lang['contact_extension_country'] = 'Country';
+$lang['contact_extension_fax_number'] = 'Fax Number';
+$lang['contact_extension_mobile_number'] = 'Nọmba ekwentị mkpanaka';
+$lang['contact_extension_organization'] = 'Organization';
+$lang['contact_extension_postal_code'] = 'Koodu Nzipu ozi';
+$lang['contact_extension_post_office_box'] = 'Postal Office Box';
+$lang['contact_extension_region'] = 'Region';
+$lang['contact_extension_room_number'] = 'ụlọ Number';
+$lang['contact_extension_street'] = 'Street';
+$lang['contact_extension_telephone_number'] = 'ekwentị Number';
+$lang['contact_extension_unit'] = 'unit';
+$lang['contact_extension_city_is_invalid'] = 'City bụ ghara ịdị irè.';
+$lang['contact_extension_country_is_invalid'] = 'Country bụ ghara ịdị irè.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Nọmba faksị bụ ghara ịdị irè.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Mobile nọmba bụ ghara ịdị irè.';
+$lang['contact_extension_organization_is_invalid'] = 'Organization bụ ghara ịdị irè.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Post ụlọ ọrụ igbe bụ ghara ịdị irè.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Koodu nzi ozi bụ ghara ịdị irè.';
+$lang['contact_extension_region_is_invalid'] = 'Region bụ ghara ịdị irè.';
+$lang['contact_extension_room_number_is_invalid'] = 'Nọmba ime ụlọ bụ ghara ịdị irè.';
+$lang['contact_extension_street_is_invalid'] = 'Street bụ ghara ịdị irè.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Nọmba ekwentị bụ ghara ịdị irè.';
+$lang['contact_extension_unit_is_invalid'] = 'Unit bụ ghara ịdị irè.';
