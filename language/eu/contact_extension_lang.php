04746f4ad6c800250ccd15c7cf68daf99c55ca79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 09:33:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Harremanetan luzapena Erabiltzaile establezimendu direktorioa hedatzen adibidez, telefono zenbakia, mugikorra, eta helbidea.';
+$lang['contact_extension_app_name'] = 'Extension Harremanetarako';
+$lang['contact_extension_contact_account_extension'] = 'Harremanetan kontua Extension';
+$lang['contact_extension_contact'] = 'Harremanetarako';
+$lang['contact_extension_city'] = 'hiria';
+$lang['contact_extension_country'] = 'Herriko';
+$lang['contact_extension_fax_number'] = 'Fax zenbakia';
+$lang['contact_extension_mobile_number'] = 'Telefono zenbakia';
+$lang['contact_extension_organization'] = 'Erakundea';
+$lang['contact_extension_postal_code'] = 'posta kodea';
+$lang['contact_extension_post_office_box'] = 'Postal Office Box';
+$lang['contact_extension_region'] = 'Region';
+$lang['contact_extension_room_number'] = 'Gela kopurua';
+$lang['contact_extension_street'] = 'Street';
+$lang['contact_extension_telephone_number'] = 'Telefono zenbakia';
+$lang['contact_extension_unit'] = 'unitatea';
+$lang['contact_extension_city_is_invalid'] = 'Hiria baliogabea da.';
+$lang['contact_extension_country_is_invalid'] = 'Herriko baliogabea da.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Fax-zenbakia baliogabea da.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Mugikorra zenbakia baliogabea da.';
+$lang['contact_extension_organization_is_invalid'] = 'Erakundeak baliogabea da.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Post Office kutxa baliogabea da.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Posta-kodea baliogabea da.';
+$lang['contact_extension_region_is_invalid'] = 'Eskualde baliogabea da.';
+$lang['contact_extension_room_number_is_invalid'] = 'Room zenbakia baliogabea da.';
+$lang['contact_extension_street_is_invalid'] = 'Street baliogabea da.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Telefono zenbakia baliogabea da.';
+$lang['contact_extension_unit_is_invalid'] = 'Unitate baliogabea da.';
