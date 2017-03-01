e6472c842fd5eead230c9c16549360cc2b2ed67f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:20:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Contact extensio usque in phone numerus presul cum user volutpat ut, mobilia, oratio.';
+$lang['contact_extension_app_name'] = 'Contact extensio';
+$lang['contact_extension_contact_account_extension'] = 'Account Contact extensio';
+$lang['contact_extension_contact'] = 'Contact';
+$lang['contact_extension_city'] = 'Urbs';
+$lang['contact_extension_country'] = 'Patriam';
+$lang['contact_extension_fax_number'] = 'Fax Number';
+$lang['contact_extension_mobile_number'] = 'Mobile numerus';
+$lang['contact_extension_organization'] = 'organization';
+$lang['contact_extension_postal_code'] = 'Zip Code';
+$lang['contact_extension_post_office_box'] = 'Praesent Officium Box';
+$lang['contact_extension_region'] = 'Region';
+$lang['contact_extension_room_number'] = 'cubiculum';
+$lang['contact_extension_street'] = 'via';
+$lang['contact_extension_telephone_number'] = 'Series numerorum telephonica';
+$lang['contact_extension_unit'] = 'Unit';
+$lang['contact_extension_city_is_invalid'] = 'Est invalidum.';
+$lang['contact_extension_country_is_invalid'] = 'Patria est invalidum.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Fax invalida est numerus.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Mobile invalida est numerus.';
+$lang['contact_extension_organization_is_invalid'] = 'Organization est invalidum.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Post arca archa officium, invalidum est.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Curabitur dolor sit irrita.';
+$lang['contact_extension_region_is_invalid'] = 'Region, invalidum est.';
+$lang['contact_extension_room_number_is_invalid'] = 'Cubiculum est invalidum.';
+$lang['contact_extension_street_is_invalid'] = 'Street est invalidum.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Phone numerus est invalidum.';
+$lang['contact_extension_unit_is_invalid'] = 'Unit est invalidum.';
