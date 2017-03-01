a9405dc040c8813469884294cceab5356417fb06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 09:30:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'The Կապ Extension երկարացնում է ցուցակում օգտվողին կոնտակտային տեղեկություններ, ինչպիսիք են հեռախոսահամարով, բջջայինի եւ վայրը.';
+$lang['contact_extension_app_name'] = 'Հետադարձ կապ Extension';
+$lang['contact_extension_contact_account_extension'] = 'Հետադարձ կապ Հաշվի Extension';
+$lang['contact_extension_contact'] = 'Կապ';
+$lang['contact_extension_city'] = 'քաղաք';
+$lang['contact_extension_country'] = 'երկիր';
+$lang['contact_extension_fax_number'] = 'ֆաքսի համարը';
+$lang['contact_extension_mobile_number'] = 'Բջջայինի համար';
+$lang['contact_extension_organization'] = 'կազմակերպություն';
+$lang['contact_extension_postal_code'] = 'փոստային ինդեքս';
+$lang['contact_extension_post_office_box'] = 'Փոստային Office Box';
+$lang['contact_extension_region'] = 'մարզ';
+$lang['contact_extension_room_number'] = 'Սենյակի համար';
+$lang['contact_extension_street'] = 'փողոց';
+$lang['contact_extension_telephone_number'] = 'Հեռախոսահամար';
+$lang['contact_extension_unit'] = 'միավոր';
+$lang['contact_extension_city_is_invalid'] = 'City անվավեր է.';
+$lang['contact_extension_country_is_invalid'] = 'Երկիր անվավեր է:';
+$lang['contact_extension_fax_number_is_invalid'] = 'Ֆաքսի համարը անվավեր է:';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Բջջային համարը անվավեր է:';
+$lang['contact_extension_organization_is_invalid'] = 'Կազմակերպությունը անվավեր է:';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Փոստատներ արկղ է անվավեր:';
+$lang['contact_extension_postal_code_is_invalid'] = 'Փոստային կոդը անվավեր է:';
+$lang['contact_extension_region_is_invalid'] = 'Տարածաշրջան անվավեր է:';
+$lang['contact_extension_room_number_is_invalid'] = 'Համարը անվավեր է:';
+$lang['contact_extension_street_is_invalid'] = 'Street անվավեր է:';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Հեռախոսահամարն է անվավեր:';
+$lang['contact_extension_unit_is_invalid'] = 'Unit է անվավեր:';
