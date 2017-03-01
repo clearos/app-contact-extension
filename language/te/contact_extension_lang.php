7bfd601d41870bd3871a9a7b0b4a9ad9f8e4456f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:57:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'సంప్రదించండి పొడిగింపు వంటి ఫోన్ నంబర్, మొబైల్, మరియు చిరునామా యూజర్ సంప్రదింపు సమాచారం డైరెక్టరీ విస్తరించింది.';
+$lang['contact_extension_app_name'] = 'సంప్రదించండి పొడిగింపు';
+$lang['contact_extension_contact_account_extension'] = 'సంప్రదించండి ఖాతా పొడిగింపు';
+$lang['contact_extension_contact'] = 'సంప్రదించండి';
+$lang['contact_extension_city'] = 'సిటీ';
+$lang['contact_extension_country'] = 'దేశం';
+$lang['contact_extension_fax_number'] = 'ఫ్యాక్స్ సంఖ్య';
+$lang['contact_extension_mobile_number'] = 'మొబైల్ నంబర్';
+$lang['contact_extension_organization'] = 'సంస్థ';
+$lang['contact_extension_postal_code'] = 'పోస్టల్ కోడ్';
+$lang['contact_extension_post_office_box'] = 'తపాలా శాఖ కార్యాలయం బాక్స్';
+$lang['contact_extension_region'] = 'ప్రాంతం';
+$lang['contact_extension_room_number'] = 'గది సంఖ్య';
+$lang['contact_extension_street'] = 'వీధి';
+$lang['contact_extension_telephone_number'] = 'టెలిఫోన్ సంఖ్య';
+$lang['contact_extension_unit'] = 'యూనిట్';
+$lang['contact_extension_city_is_invalid'] = 'సిటీ చెల్లదు.';
+$lang['contact_extension_country_is_invalid'] = 'దేశం చెల్లనిది.';
+$lang['contact_extension_fax_number_is_invalid'] = 'ఫ్యాక్స్ నంబర్ చెల్లదు.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'మీ మైబైల్ నెంబర్ చెల్లదు.';
+$lang['contact_extension_organization_is_invalid'] = 'సంస్థ చెల్లదు.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'పోస్టాఫీస్ బాక్స్ చెల్లనిది.';
+$lang['contact_extension_postal_code_is_invalid'] = 'పోస్టల్ కోడ్ చెల్లదు.';
+$lang['contact_extension_region_is_invalid'] = 'ప్రాంతం చెల్లదు.';
+$lang['contact_extension_room_number_is_invalid'] = 'గది సంఖ్య చెల్లదు.';
+$lang['contact_extension_street_is_invalid'] = 'స్ట్రీట్ చెల్లదు.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'టెలిఫోన్ నంబర్ చెల్లదు.';
+$lang['contact_extension_unit_is_invalid'] = 'యూనిట్ చెల్లదు.';
