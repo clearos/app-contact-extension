5fe8e7c0e5128f853cdaab9229208eb6470fe35e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:57:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Việc mở rộng Liên hệ mở rộng các thư mục với thông tin người dùng liên lạc như số điện thoại, di động và địa chỉ.';
+$lang['contact_extension_app_name'] = 'Liên hệ mở rộng';
+$lang['contact_extension_contact_account_extension'] = 'Mở rộng Tài khoản Liên hệ';
+$lang['contact_extension_contact'] = 'Tiếp xúc';
+$lang['contact_extension_city'] = 'thành phố';
+$lang['contact_extension_country'] = 'Quốc gia';
+$lang['contact_extension_fax_number'] = 'Số fax';
+$lang['contact_extension_mobile_number'] = 'Số điện thoại';
+$lang['contact_extension_organization'] = 'Cơ quan';
+$lang['contact_extension_postal_code'] = 'Mã bưu điện';
+$lang['contact_extension_post_office_box'] = 'Bưu Box Office';
+$lang['contact_extension_region'] = 'Khu vực';
+$lang['contact_extension_room_number'] = 'Số phòng';
+$lang['contact_extension_street'] = 'đường phố';
+$lang['contact_extension_telephone_number'] = 'Số điện thoại';
+$lang['contact_extension_unit'] = 'Đơn vị';
+$lang['contact_extension_city_is_invalid'] = 'Thành phố là không hợp lệ.';
+$lang['contact_extension_country_is_invalid'] = 'Nước không hợp lệ.';
+$lang['contact_extension_fax_number_is_invalid'] = 'số fax là không hợp lệ.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'số điện thoại di động là không hợp lệ.';
+$lang['contact_extension_organization_is_invalid'] = 'Tổ chức là không hợp lệ.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'hộp bưu điện là không hợp lệ.';
+$lang['contact_extension_postal_code_is_invalid'] = 'mã bưu chính là không hợp lệ.';
+$lang['contact_extension_region_is_invalid'] = 'Khu vực là không hợp lệ.';
+$lang['contact_extension_room_number_is_invalid'] = 'Số phòng là không hợp lệ.';
+$lang['contact_extension_street_is_invalid'] = 'Street là không hợp lệ.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Số điện thoại không hợp lệ.';
+$lang['contact_extension_unit_is_invalid'] = 'Đơn vị là không hợp lệ.';
