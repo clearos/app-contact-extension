8f06a1a163db9751773823cd27d6620a2c2a3e8a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 09:54:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Le contact Extension étend le répertoire avec les informations de contact de l&#39;utilisateur telles que le numéro de téléphone, mobile, et l&#39;adresse.';
+$lang['contact_extension_app_name'] = 'Contactez-Extension';
+$lang['contact_extension_contact_account_extension'] = 'Contacter compte Extension';
+$lang['contact_extension_contact'] = 'Contact';
+$lang['contact_extension_city'] = 'Ville';
+$lang['contact_extension_country'] = 'Pays';
+$lang['contact_extension_fax_number'] = 'Numéro de fax';
+$lang['contact_extension_mobile_number'] = 'Numéro de portable';
+$lang['contact_extension_organization'] = 'Organisation';
+$lang['contact_extension_postal_code'] = 'code postal';
+$lang['contact_extension_post_office_box'] = 'Bureau postal Box';
+$lang['contact_extension_region'] = 'Région';
+$lang['contact_extension_room_number'] = 'Numéro de chambre';
+$lang['contact_extension_street'] = 'rue';
+$lang['contact_extension_telephone_number'] = 'Numéro de téléphone';
+$lang['contact_extension_unit'] = 'Unité';
+$lang['contact_extension_city_is_invalid'] = 'Ville est invalide.';
+$lang['contact_extension_country_is_invalid'] = 'Pays est invalide.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Numéro de fax est invalide.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Numéro de téléphone mobile est invalide.';
+$lang['contact_extension_organization_is_invalid'] = 'Organisation est invalide.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Boîte postale est invalide.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Code postal est invalide.';
+$lang['contact_extension_region_is_invalid'] = 'Région est invalide.';
+$lang['contact_extension_room_number_is_invalid'] = 'numéro de chambre est invalide.';
+$lang['contact_extension_street_is_invalid'] = 'Street est invalide.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Numéro de téléphone est invalide.';
+$lang['contact_extension_unit_is_invalid'] = 'Unité est invalide.';
