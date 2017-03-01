bf7974c06a16c81b836efd350455f5c3a24a26a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:36:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'De Contact Extension breidt de directory met gebruiksvriendelijke contactgegevens zoals telefoonnummer, mobiel, en adres.';
+$lang['contact_extension_app_name'] = 'Contact Uitbreiding';
+$lang['contact_extension_contact_account_extension'] = 'Contact Account Extension';
+$lang['contact_extension_contact'] = 'Contact';
+$lang['contact_extension_city'] = 'stad';
+$lang['contact_extension_country'] = 'land';
+$lang['contact_extension_fax_number'] = 'Faxnummer';
+$lang['contact_extension_mobile_number'] = 'Mobiel nummer';
+$lang['contact_extension_organization'] = 'Organisatie';
+$lang['contact_extension_postal_code'] = 'Postcode';
+$lang['contact_extension_post_office_box'] = 'Postal Box Office';
+$lang['contact_extension_region'] = 'Regio';
+$lang['contact_extension_room_number'] = 'Kamernummer';
+$lang['contact_extension_street'] = 'Straat';
+$lang['contact_extension_telephone_number'] = 'Telefoon nummer';
+$lang['contact_extension_unit'] = 'Eenheid';
+$lang['contact_extension_city_is_invalid'] = 'City is ongeldig.';
+$lang['contact_extension_country_is_invalid'] = 'Country is ongeldig.';
+$lang['contact_extension_fax_number_is_invalid'] = 'Faxnummer is ongeldig.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Mobiel nummer is ongeldig.';
+$lang['contact_extension_organization_is_invalid'] = 'Organisatie is ongeldig.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Postbus is ongeldig.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Postcode is ongeldig.';
+$lang['contact_extension_region_is_invalid'] = 'Region is ongeldig.';
+$lang['contact_extension_room_number_is_invalid'] = 'Kamernummer is ongeldig.';
+$lang['contact_extension_street_is_invalid'] = 'Street is ongeldig.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Telefoonnummer is ongeldig.';
+$lang['contact_extension_unit_is_invalid'] = 'Unit is ongeldig.';
