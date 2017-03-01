7beb5304641fb9068567bcbb609f7bfa2a704d67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:29:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'ബന്ധപ്പെടുക വിപുലീകരണ ഫോൺ നമ്പർ, മൊബൈൽ, വിലാസം പോലുള്ള ബന്ധപ്പെടാനുള്ള വിവരങ്ങൾ ഡയറക്ടറി വ്യാപിച്ചിരിക്കുന്നു.';
+$lang['contact_extension_app_name'] = 'ബന്ധപ്പെടുക വിപുലീകരണം';
+$lang['contact_extension_contact_account_extension'] = 'അക്കൗണ്ട് വിപുലീകരണം ബന്ധപ്പെടുക';
+$lang['contact_extension_contact'] = 'ബന്ധപ്പെടുക';
+$lang['contact_extension_city'] = 'നഗരം';
+$lang['contact_extension_country'] = 'രാജ്യം';
+$lang['contact_extension_fax_number'] = 'ഫാക്സ് നമ്പർ';
+$lang['contact_extension_mobile_number'] = 'മൊബൈൽ നമ്പർ';
+$lang['contact_extension_organization'] = 'സംഘടന';
+$lang['contact_extension_postal_code'] = 'തപാൽ കോഡ്';
+$lang['contact_extension_post_office_box'] = 'തപാൽ ഓഫീസ് ബോക്സ്';
+$lang['contact_extension_region'] = 'പ്രദേശം';
+$lang['contact_extension_room_number'] = 'മുറി നമ്പർ';
+$lang['contact_extension_street'] = 'തെരുവ്';
+$lang['contact_extension_telephone_number'] = 'ടെലിഫോൺ നമ്പർ';
+$lang['contact_extension_unit'] = 'ഘടകം';
+$lang['contact_extension_city_is_invalid'] = 'സിറ്റി അസാധുവാണ്.';
+$lang['contact_extension_country_is_invalid'] = 'രാജ്യം അസാധുവാണ്.';
+$lang['contact_extension_fax_number_is_invalid'] = 'ഫാക്സ് നമ്പർ അസാധുവാണ്.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'മൊബൈൽ നമ്പർ അസാധുവാണ്.';
+$lang['contact_extension_organization_is_invalid'] = 'ഓർഗനൈസേഷൻ അസാധുവാണ്.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'പോസ്റ്റ് ഓഫീസ് ബോക്സ് അസാധുവാണ്.';
+$lang['contact_extension_postal_code_is_invalid'] = 'തപാൽ കോഡ് അസാധുവാണ്.';
+$lang['contact_extension_region_is_invalid'] = 'മേഖല അസാധുവാണ്.';
+$lang['contact_extension_room_number_is_invalid'] = 'റൂം നമ്പർ അസാധുവാണ്.';
+$lang['contact_extension_street_is_invalid'] = 'തെരുവ് അസാധുവാണ്.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'ടെലിഫോൺ നമ്പർ അസാധുവാണ്.';
+$lang['contact_extension_unit_is_invalid'] = 'ഭാഗം അസാധുവാണ്.';
