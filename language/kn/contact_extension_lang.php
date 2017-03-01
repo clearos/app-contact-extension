34f425a75fade723e6af609726ed2ce2dae275d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:18:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'ಸಂಪರ್ಕ ವಿಸ್ತರಣೆ ಫೋನ್ ಸಂಖ್ಯೆ, ಮೊಬೈಲ್, ಮತ್ತು ವಿಳಾಸ ಬಳಕೆದಾರ ಸಂಪರ್ಕ ಮಾಹಿತಿಯನ್ನು ಕೋಶವನ್ನು ವಿಸ್ತರಿಸುತ್ತದೆ.';
+$lang['contact_extension_app_name'] = 'ಸಂಪರ್ಕ ವಿಸ್ತರಣೆ';
+$lang['contact_extension_contact_account_extension'] = 'ಸಂಪರ್ಕ ಖಾತೆ ವಿಸ್ತರಣೆ';
+$lang['contact_extension_contact'] = 'ಸಂಪರ್ಕಿಸಿ';
+$lang['contact_extension_city'] = 'ಸಿಟಿ';
+$lang['contact_extension_country'] = 'ದೇಶದ';
+$lang['contact_extension_fax_number'] = 'ಫ್ಯಾಕ್ಸ್ ಸಂಖ್ಯೆ';
+$lang['contact_extension_mobile_number'] = 'ಮೊಬೈಲ್ ನಂಬರ';
+$lang['contact_extension_organization'] = 'ಸಂಸ್ಥೆ';
+$lang['contact_extension_postal_code'] = 'ಅಂಚೆ ಕೋಡ್';
+$lang['contact_extension_post_office_box'] = 'ಅಂಚೆ ಕಚೇರಿ ಬಾಕ್ಸ್';
+$lang['contact_extension_region'] = 'ಪ್ರದೇಶ';
+$lang['contact_extension_room_number'] = 'ಕೋಣೆ ಸಂಖ್ಯೆ';
+$lang['contact_extension_street'] = 'ಸ್ಟ್ರೀಟ್';
+$lang['contact_extension_telephone_number'] = 'ದೂರವಾಣಿ ಸಂಖ್ಯೆ';
+$lang['contact_extension_unit'] = 'ಘಟಕ';
+$lang['contact_extension_city_is_invalid'] = 'ಸಿಟಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_country_is_invalid'] = 'ರಾಷ್ಟ್ರ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_fax_number_is_invalid'] = 'ಫ್ಯಾಕ್ಸ್ ಸಂಖ್ಯೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'ಮೊಬೈಲ್ ಸಂಖ್ಯೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_organization_is_invalid'] = 'ಸಂಸ್ಥೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'ಪೋಸ್ಟ್ ಆಫೀಸ್ ಬಾಕ್ಸ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_postal_code_is_invalid'] = 'ಪೋಸ್ಟಲ್ ಕೋಡ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_region_is_invalid'] = 'ಪ್ರದೇಶ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_room_number_is_invalid'] = 'ಕೊಠಡಿ ಸಂಖ್ಯೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_street_is_invalid'] = 'ಸ್ಟ್ರೀಟ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'ದೂರವಾಣಿ ಸಂಖ್ಯೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['contact_extension_unit_is_invalid'] = 'ಘಟಕ ಅಮಾನ್ಯವಾಗಿದೆ.';
