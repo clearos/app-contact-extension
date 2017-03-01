7d1911b71ab180b8c2dd87abec1096a03341a505
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:57:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Contact Extension usinika lwemibhalo ngolwazi contact lomsebenzisi ezifana nenombolo yocingo, mobile, kanye nekheli.';
+$lang['contact_extension_app_name'] = 'Contact Extension';
+$lang['contact_extension_contact_account_extension'] = 'Contact-Akhawunti Extension';
+$lang['contact_extension_contact'] = 'Contact';
+$lang['contact_extension_city'] = 'City';
+$lang['contact_extension_country'] = 'Izwe';
+$lang['contact_extension_fax_number'] = 'Inombolo Yesikhahlamezi';
+$lang['contact_extension_mobile_number'] = 'Inombolo yeselula';
+$lang['contact_extension_organization'] = 'Inhlangano';
+$lang['contact_extension_postal_code'] = 'I-Postal Code';
+$lang['contact_extension_post_office_box'] = 'Leposi Office Box';
+$lang['contact_extension_region'] = 'Region';
+$lang['contact_extension_room_number'] = 'Inombolo Room';
+$lang['contact_extension_street'] = 'Street';
+$lang['contact_extension_telephone_number'] = 'Inombolo yocingo';
+$lang['contact_extension_unit'] = 'Unit';
+$lang['contact_extension_city_is_invalid'] = 'City ayivumelekile.';
+$lang['contact_extension_country_is_invalid'] = 'Country ayivumelekile.';
+$lang['contact_extension_fax_number_is_invalid'] = 'inombolo yefeksi ayivumelekile.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'Inombolo yeselula engavumelekile.';
+$lang['contact_extension_organization_is_invalid'] = 'Inhlangano ayivumelekile.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'Eposini ibhokisi ayivumelekile.';
+$lang['contact_extension_postal_code_is_invalid'] = 'Ikhodi yeposi ayivumelekile.';
+$lang['contact_extension_region_is_invalid'] = 'Region ayivumelekile.';
+$lang['contact_extension_room_number_is_invalid'] = 'Inombolo Room ayivumelekile.';
+$lang['contact_extension_street_is_invalid'] = 'Street ayivumelekile.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Inombolo yocingo ayivumelekile.';
+$lang['contact_extension_unit_is_invalid'] = 'Unit ayivumelekile.';
