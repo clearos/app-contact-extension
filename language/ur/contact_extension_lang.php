d7333bd77b78ae855b9e3c307ec4c6b9390954d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 14:56:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'رابطہ توسیع جیسے فون نمبر، موبائل، اور پتہ صارف کے رابطے کی معلومات کے ساتھ ڈائریکٹری میں توسیع.';
+$lang['contact_extension_app_name'] = 'رابطہ توسیع';
+$lang['contact_extension_contact_account_extension'] = 'رابطہ اکاؤنٹ توسیع';
+$lang['contact_extension_contact'] = 'رابطہ کریں';
+$lang['contact_extension_city'] = 'شہر';
+$lang['contact_extension_country'] = 'ملک';
+$lang['contact_extension_fax_number'] = 'فیکس نمبر';
+$lang['contact_extension_mobile_number'] = 'موبائل فون کانمبر';
+$lang['contact_extension_organization'] = 'ادارہ';
+$lang['contact_extension_postal_code'] = 'ڈاک کامخصوص نمبر';
+$lang['contact_extension_post_office_box'] = 'پوسٹل آفس بکس';
+$lang['contact_extension_region'] = 'علاقہ';
+$lang['contact_extension_room_number'] = 'کمرہ نمبر';
+$lang['contact_extension_street'] = 'سٹریٹ';
+$lang['contact_extension_telephone_number'] = 'ٹیلی فون نمبر';
+$lang['contact_extension_unit'] = 'اکائی';
+$lang['contact_extension_city_is_invalid'] = 'سٹی غلط ہے.';
+$lang['contact_extension_country_is_invalid'] = 'ملک غلط ہے.';
+$lang['contact_extension_fax_number_is_invalid'] = 'فیکس نمبر غلط ہے.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'موبائل نمبر غلط ہے.';
+$lang['contact_extension_organization_is_invalid'] = 'تنظیم غلط ہے.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'پوسٹ آفس باکس غلط ہے.';
+$lang['contact_extension_postal_code_is_invalid'] = 'پوسٹل کوڈ درست نہیں ہے.';
+$lang['contact_extension_region_is_invalid'] = 'علاقہ غلط ہے.';
+$lang['contact_extension_room_number_is_invalid'] = 'کمرہ نمبر غلط ہے.';
+$lang['contact_extension_street_is_invalid'] = 'سٹریٹ غلط ہے.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'ٹیلی فون نمبر غلط ہے.';
+$lang['contact_extension_unit_is_invalid'] = 'یونٹ غلط ہے.';
