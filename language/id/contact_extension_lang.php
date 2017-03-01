380e351d5bbf67b5ec1514cbfa76b307b4cab55d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 10:06:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/contact_extension_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['contact_extension_app_description'] = 'Contact Ekstensi meluas direktori dengan informasi kontak pengguna seperti nomor telepon, ponsel, dan alamat.';
+$lang['contact_extension_app_name'] = 'Hubungi Ekstensi';
+$lang['contact_extension_contact_account_extension'] = 'Kontak Akun Ekstensi';
+$lang['contact_extension_contact'] = 'Kontak';
+$lang['contact_extension_city'] = 'Kota';
+$lang['contact_extension_country'] = 'Negara';
+$lang['contact_extension_fax_number'] = 'Nomor fax';
+$lang['contact_extension_mobile_number'] = 'Nomor handphone';
+$lang['contact_extension_organization'] = 'Organisasi';
+$lang['contact_extension_postal_code'] = 'Kode Pos';
+$lang['contact_extension_post_office_box'] = 'Pos Box Office';
+$lang['contact_extension_region'] = 'Wilayah';
+$lang['contact_extension_room_number'] = 'Nomor kamar';
+$lang['contact_extension_street'] = 'jalan';
+$lang['contact_extension_telephone_number'] = 'Nomor telepon';
+$lang['contact_extension_unit'] = 'Satuan';
+$lang['contact_extension_city_is_invalid'] = 'Kota tidak valid.';
+$lang['contact_extension_country_is_invalid'] = 'Negara tidak valid.';
+$lang['contact_extension_fax_number_is_invalid'] = 'nomor faks tidak valid.';
+$lang['contact_extension_mobile_number_is_invalid'] = 'nomor ponsel tidak valid.';
+$lang['contact_extension_organization_is_invalid'] = 'Organisasi tidak valid.';
+$lang['contact_extension_post_office_box_is_invalid'] = 'kotak pos tidak valid.';
+$lang['contact_extension_postal_code_is_invalid'] = 'kode pos tidak valid.';
+$lang['contact_extension_region_is_invalid'] = 'Wilayah tidak valid.';
+$lang['contact_extension_room_number_is_invalid'] = 'nomor kamar tidak valid.';
+$lang['contact_extension_street_is_invalid'] = 'Jalan tidak valid.';
+$lang['contact_extension_telephone_number_is_invalid'] = 'Nomor telepon tidak valid.';
+$lang['contact_extension_unit_is_invalid'] = 'Unit tidak valid.';
